ce9a260de059f9e0d12289f5d25b49b86f0117c8
Hallie Arno <60670464+hallie00@users.noreply.github.com>
2020-02-07 12:04:43 -0500

    Update exponential_growth_preliminary.py

--- exponential_growth_preliminary.py
+++ exponential_growth_preliminary.py
@@ -1,35 +1,43 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Feb  4 15:27:41 2020
+Created on Fri Feb  7 11:19:01 2020
 
-@author: Andrea
+@author: hallie
 """
 
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  4 14:46:34 2020
+@author: Andrea
+"""
 
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import odeint
 
-Initial_Pop = 2 #g carbon
+Initial_Pop = [2, 20, 45] #g carbon
 photosynth = 1.8 #g*cal per cm squared per hour, we only sort of trust this number
 resp = 0.1 #we made this number up but it seems reasonable, also it needs to depend on temp
 graze = 0.05 # g carbon
 endtime = 5  #hours
 
 time = np.linspace(0,endtime,50)
-
-def f(X,time):
-    Pop = X[0]
-    dPop = Pop*(photosynth-resp-graze) #Riley, Gordon A. "Factors controlling phytoplankton populations on Georges Bank." J. mar. Res 6.1 (1946): 54-73.
-    return([dPop])
-
-Initial_condition = 20 #g carbon
-
-hab_solution = odeint(f, Initial_Pop, time)
-EndPop = [vector[0] for vector in hab_solution]
-
 plt.figure()
-plt.plot(time,EndPop)
-plt.xlabel("time (hours)")
-plt.ylabel("Phytoplankton (Grams of Carbon)")
-plt.title("Exponential HAB growth")
+
+for i in Initial_Pop:
+    def f(X,time):
+        Pop = X[0]
+        dZooGraze = graze*Pop
+        dPop = Pop*(photosynth-resp-dZooGraze) #Riley, Gordon A. "Factors controlling phytoplankton populations on Georges Bank." J. mar. Res 6.1 (1946): 54-73.
+        return([dPop])
+
+
+    hab_solution = odeint(f, i, time)
+    EndPop = [vector[0] for vector in hab_solution]
+
+
+    plt.plot(time,EndPop)
+    plt.xlabel("time (hours)")
+    plt.ylabel("Phytoplankton (Grams of Carbon)")
+    plt.title("HAB growth mediated by strong predation")
